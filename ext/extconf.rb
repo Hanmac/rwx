197af4bcfc39edd8f999aa3a8c61aabeb6f2c0a6
Hanmac <hanmac@gmx.de>
2013-02-15 08:21:52 +0100

    Alter the extconf.rb to let it work with ruby-trunk

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,30 +1,38 @@
 require "mkmf"
-$CFLAGS << " -fvisibility-inlines-hidden -x c++ -g -Wall "
-$CPPFLAGS << " -fvisibility-inlines-hidden -g "
-$LDFLAGS << " -fvisibility-inlines-hidden "
+
 
 dir_config "rwx"
 
 if(wx_config = find_executable('wx-config'))
-	$CFLAGS << `#{wx_config} --cflags`.chomp + " " + `#{wx_config} --cxxflags`.chomp
-	$CPPFLAGS << `#{wx_config} --cppflags`
-	$LDFLAGS << `#{wx_config} --libs all`
+
 	
 	case `#{wx_config} --basename`
 	when /gtk2/
-		p "gtk2"
-		pkg_config("gdk-x11-2.0")
-		pkg_config("gtk+-x11-2.0")
+		#pkg_config("gdk-x11-2.0")
+		#pkg_config("gtk+-x11-2.0")
 	when /gtk3/
-		p "gtk3"
 		pkg_config("gtk-x11-3.0")
 		pkg_config("gtk+-x11-3.0")
 	end
-	
+
+	$CFLAGS << " -fvisibility-inlines-hidden -x c++ -g -Wall "
+	$CPPFLAGS << " -fvisibility-inlines-hidden -g "
+	$LDFLAGS << " -fvisibility-inlines-hidden "
+
+
+	$CFLAGS << `#{wx_config} --cflags`.chomp + " " + `#{wx_config} --cxxflags`.chomp
+	$CPPFLAGS << `#{wx_config} --cppflags`.chomp
+	$LDFLAGS << `#{wx_config} --libs all`.chomp
+
+
 	
 else
 		abort("wx-config executable not found")
 
 end
+
+#drop some of the warn flags because they are not valid for C++
+CONFIG["warnflags"].gsub!("-Wdeclaration-after-statement","")
+CONFIG["warnflags"].gsub!("-Wimplicit-function-declaration","")
 
 create_makefile "rwx"
