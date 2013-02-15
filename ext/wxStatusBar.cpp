57909bc3411d098bdaf362417bd55db1ee398b5b
Hanmac <hanmac@gmx.de>
2013-02-15 08:21:12 +0100

    Begin with the Statusbar functions to access the StatusBarPanes

--- ext/wxStatusBar.cpp
+++ ext/wxStatusBar.cpp
@@ -19,6 +19,28 @@ namespace StatusBar {
 
 APP_PROTECT(wxStatusBar)
 
+
+macro_attr(StatusText,wxString)
+macro_attr(FieldsCount,int)
+
+
+VALUE _getStatusText2(VALUE self,VALUE num)
+{
+	if(NUM2INT(num) < _self->GetFieldsCount())
+		return wrap(_self->GetStatusText(NUM2INT(num)));
+	return Qnil;
+}
+
+VALUE _setStatusText2(VALUE self,VALUE str,VALUE num)
+{
+	if(NUM2INT(num) < _self->GetFieldsCount())
+		_self->SetStatusText(unwrap<wxString>(str),NUM2INT(num));
+	//const_cast<wxStatusBarPane&>(_self->GetField(NUM2INT(num))).SetText(unwrap<wxString>(str));
+	//_self->Update();
+	return Qnil;
+}
+
+
 }
 }
 #endif
@@ -28,6 +50,12 @@ DLL_LOCAL void Init_WXStatusBar(VALUE rb_mWX)
 	using namespace RubyWX::StatusBar;
 	rb_cWXStatusBar = rb_define_class_under(rb_mWX,"StatusBar",rb_cWXControl);
 	rb_define_alloc_func(rb_cWXStatusBar,_alloc);
+
+	rb_define_attr_method(rb_cWXStatusBar,"status_text",_getStatusText,_setStatusText);
+	rb_define_attr_method(rb_cWXStatusBar,"fields_count",_getFieldsCount,_setFieldsCount);
+
+	rb_define_method(rb_cWXStatusBar,"get_status_text",RUBY_METHOD_FUNC(_getStatusText2),1);
+	rb_define_method(rb_cWXStatusBar,"set_status_text",RUBY_METHOD_FUNC(_setStatusText2),2);
 
 	registerInfo<wxStatusBar>(rb_cWXStatusBar);
 #endif
