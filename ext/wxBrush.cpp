2a105a9d502b78b1a4ee6340a6d8663025f4179e
Hanmac <hanmac@gmx.de>
2013-12-05 16:31:34 +0100

    WxBrush: add initialize, initialize_copy and marshal_ methods

--- ext/wxBrush.cpp
+++ ext/wxBrush.cpp
@@ -28,24 +28,118 @@ macro_attr(Colour,wxColour)
 macro_attr_enum(Style,wxBrushStyle)
 macro_attr(Stipple,wxBitmap)
 
+singlereturn(IsHatch)
+
+
+/*
+ * call-seq:
+ *   Brush.new(color[,style])
+ *
+ * creates a new Brush Object.
+*/
+DLL_LOCAL VALUE _initialize(int argc,VALUE *argv,VALUE self)
+{
+	VALUE color,style,blue,alpha;
+	rb_scan_args(argc, argv, "11",&color,&style,&blue,&alpha);
+	_setColour(self,color);
+	if(!NIL_P(style))
+		_setStyle(self,style);
+	return self;
+}
+
+/*
+*/
+DLL_LOCAL VALUE _initialize_copy(VALUE self, VALUE other)
+{
+	VALUE result = rb_call_super(1,&other);
+	_setColour(self,_getColour(other));
+	_setStyle(self,_getStyle(other));
+	_setStipple(self,_getStipple(other));
+	return result;
+}
+
+
+/*
+ * call-seq:
+ *   marshal_dump -> Array
+ *
+ * Provides marshalling support for use by the Marshal library.
+ * ===Return value
+ * Array
+ */
+DLL_LOCAL VALUE _marshal_dump(VALUE self)
+{
+    VALUE ptr[3];
+    ptr[0] = _getColour(self);
+    ptr[1] = _getStyle(self);
+    ptr[2] = _getStipple(self);
+    return rb_ary_new4(3, ptr);
+}
+
+/*
+ * call-seq:
+ *   marshal_load(array) -> nil
+ *
+ * Provides marshalling support for use by the Marshal library.
+ *
+ *
+ */
+DLL_LOCAL VALUE _marshal_load(VALUE self, VALUE data)
+{
+    VALUE* ptr = RARRAY_PTR(data);
+    _setColour(self, ptr[0]);
+    _setStyle(self, ptr[1]);
+    _setStipple(self, ptr[2]);
+    return Qnil;
+}
+
+
+/*
+ * call-seq:
+ *   == brush -> bool
+ *
+ * compares two brush.
+ *
+ *
+ */
+DLL_LOCAL VALUE _equal(VALUE self, VALUE other)
+{
+	return wrap((*_self) == unwrap<wxBrush>(other));
+}
+
+
 }
 }
 
 DLL_LOCAL void Init_WXBrush(VALUE rb_mWX)
 {
+#if 0
+
+	rb_define_attr(rb_cWXBrush,"color",1,1);
+	rb_define_attr(rb_cWXBrush,"style",1,1);
+	rb_define_attr(rb_cWXBrush,"stipple",1,1);
+
+#endif
+
 	using namespace RubyWX::Brush;
 	rb_cWXBrush = rb_define_class_under(rb_mWX,"Brush",rb_cObject);
 	rb_define_alloc_func(rb_cWXBrush,_alloc);
 
-//	rb_define_method(rb_cWXBrush,"initialize",RUBY_METHOD_FUNC(_initialize),-1);
-//	rb_define_private_method(rb_cWXBrush,"initialize_copy",RUBY_METHOD_FUNC(_initialize_copy),1);
-//
-//
+	rb_define_method(rb_cWXBrush,"initialize",RUBY_METHOD_FUNC(_initialize),-1);
+	rb_define_private_method(rb_cWXBrush,"initialize_copy",RUBY_METHOD_FUNC(_initialize_copy),1);
+
 	rb_define_attr_method(rb_cWXBrush,"color",_getColour,_setColour);
 	rb_define_attr_method(rb_cWXBrush,"style",_getStyle,_setStyle);
 	rb_define_attr_method(rb_cWXBrush,"stipple",_getStipple,_setStipple);
 
-//
+	rb_define_method(rb_cWXBrush,"hatch?",RUBY_METHOD_FUNC(_IsHatch),0);
+
+	rb_define_method(rb_cWXBrush,"marshal_dump",RUBY_METHOD_FUNC(_marshal_dump),0);
+	rb_define_method(rb_cWXBrush,"marshal_load",RUBY_METHOD_FUNC(_marshal_load),1);
+
+	rb_define_method(rb_cWXBrush,"==",RUBY_METHOD_FUNC(_equal),1);
+
+	//
 //	rb_define_method(rb_cWXBrush,"to_s",RUBY_METHOD_FUNC(_tos),0);
 //	rb_define_method(rb_cWXBrush,"inspect",RUBY_METHOD_FUNC(_inspect),0);
 
@@ -56,7 +150,7 @@ DLL_LOCAL void Init_WXBrush(VALUE rb_mWX)
 		->add(wxBRUSHSTYLE_STIPPLE_MASK_OPAQUE,"stipple_mask_opaque")
 		->add(wxBRUSHSTYLE_STIPPLE_MASK,"stipple_mask")
 		->add(wxBRUSHSTYLE_STIPPLE,"stipple")
-		->add(wxBRUSHSTYLE_BDIAGONAL_HATCH,"bdiagona_hatch")
+		->add(wxBRUSHSTYLE_BDIAGONAL_HATCH,"bdiagonal_hatch")
 		->add(wxBRUSHSTYLE_CROSSDIAG_HATCH,"crossdiag_hatch")
 		->add(wxBRUSHSTYLE_FDIAGONAL_HATCH,"fdiagonal_hatch")
 		->add(wxBRUSHSTYLE_CROSS_HATCH,"cross_hatch")
