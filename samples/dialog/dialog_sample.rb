8f83abc15da4dbad33fffef6d71c46a2bd9f0d70
Hanmac <hanmac@gmx.de>
2013-12-25 16:27:37 +0100

    add AboutDialog to samples/dialog

--- samples/dialog/dialog_sample.rb
+++ samples/dialog/dialog_sample.rb
@@ -1,62 +1,110 @@
 #Encoding: UTF-8
 require "../../ext/rwx"
 
+class MyAboutDialog < WX::AboutDialog
+  
+  def custom_controls
+    add_control(WX::StaticLine,:expand => true)
+    add_text("Some custom text")
+    add_control(WX::StaticLine,:expand => true)
+  end
+end
+
 class A < WX::App
+	
+	def aboutinfo_minimal
+		{
+			:name => "Dialogs Sample",
+			:version => [WX::WXVERSION,WX::WXVERSION_MINOR % 2 == 0 ? "Development" : "Stable" ],
+			:description => "This sample shows different wxWidgets dialogs",
+			:copyright	=> "(C) 1998-2006 wxWidgets dev team",
+			:developers => "Vadim Zeitlin"
+		}
+	end
+	
+	def aboutinfo_website
+		aboutinfo_minimal.merge(:web_site => ["http://www.wxwidgets.org/","wxWidgets web site"] )
+	end
+	
+	def aboutinfo_all
+		aboutinfo_website.merge(
+			:developers => "A.N. Other",
+			:doc_writers => ["First D. Writer", "Second One"],
+			:translators => "Wun Ngo Wen (Martian)",
+			:licence => <<LICENCE
+                wxWindows Library Licence, Version 3.1
+                ======================================
+
+  Copyright (c) 1998-2005 Julian Smart, Robert Roebling et al
+
+  Everyone is permitted to copy and distribute verbatim copies
+  of this licence document, but changing it is not allowed.
+
+                       WXWINDOWS LIBRARY LICENCE
+     TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+                    ...and so on and so forth...
+LICENCE
+		) {|key,old,new| Array(old) + Array(new)	}
+	end
+	
+	
 	def on_init
 	
 	
 #	
 #		#f = WX::Font.new(10,:script)
 #		#p f.faceName
-		@frame = WX::Frame.new(nil)
+		@frame = WX::Frame.new(nil,:icon => "../sample.xpm")
 	
 		@index = 0
 		
 		@frame.menubar = WX::MenuBar.new(nil) {|m|
 			m.append("&Dialogs") {|menu|
 				menu.append_normal("&Choices and selectors") {|select|
-				  
-				  select.append_normal(:single_choice,"&Single choice\tCtrl-C") {
-				    dialog = WX::SingleChoiceDialog.new(@frame,
-				      :message => "This is a small sample\nA single-choice convenience dialog",
-				      :title => "Please select a value",
-				      :choices => %w(one two three four five),
-				      :selection => 2
-				    )
-				    
-            if dialog.show_modal == :ok
-              WX::message_box(@frame,dialog.string_selection,:caption => "Got string")
-            end
-				  }
-          select.append_normal(:multi_choice,"M&ultiple choice\tCtrl-U") {
-            
-          }
-          
-          select.append_separator
-                   
-          select.append_normal(:single_choice2,"&Single choice per command") {
-            choice = WX::single_choice(
-              "This is a small sample\nA single-choice convenience dialog",
-              "Please select a value",%w(one two three four five),
-              :parent => @frame,:selection => 2
-            )
-            
-            WX::message_box(@frame,choice,:caption => "Got string")
-            
-          }
-          
-          select.append_separator
-				  
+					
+					select.append_normal(:single_choice,"&Single choice\tCtrl-C") {
+						dialog = WX::SingleChoiceDialog.new(@frame,
+							:message => "This is a small sample\nA single-choice convenience dialog",
+							:title => "Please select a value",
+							:choices => %w(one two three four five),
+							:selection => 2
+						)
+						
+						if dialog.show_modal == :ok
+							WX::message_box(@frame,dialog.string_selection,:caption => "Got string")
+						end
+					}
+					
+					select.append_normal(:multi_choice,"M&ultiple choice\tCtrl-U") {
+						
+					}
+					
+					select.append_separator
+									 
+					select.append_normal(:single_choice2,"&Single choice per command") {
+						choice = WX::single_choice(
+							"This is a small sample\nA single-choice convenience dialog",
+							"Please select a value",%w(one two three four five),
+							:parent => @frame,:selection => 2
+						)
+						
+						WX::message_box(@frame,choice,:caption => "Got string")
+						
+					}
+					
+					select.append_separator
+					
 					select.append_normal(:rearrange,"&Rearrange dialog\tCtrl-R") {
 						dialog = WX::RearrangeDialog.new(@frame,
 							:items => "A".."E", :order => 0..4, :message => "Configure the columns shown:"
 						)
 						dialog.add_extra_controls(WX::Panel) {|pan|
-						  pan.sizer = WX::BoxSizer.new {|box|
-						    box.add(WX::StaticText.new(pan))
-                box.add(WX::TextCtrl.new(pan))
-                box.add(WX::Button.new(pan, :id => :rename, :label => "&Rename"))
-						  }
+							pan.sizer = WX::BoxSizer.new {|box|
+								box.add(WX::StaticText.new(pan))
+								box.add(WX::TextCtrl.new(pan))
+								box.add(WX::Button.new(pan, :id => :rename, :label => "&Rename"))
+							}
 						}
 						dialog.show_modal
 					}
@@ -89,6 +137,22 @@ class A < WX::App
 					
 					
 				}
+			}
+			m.append("&Help") {|help|
+				help.append_normal(:about_simple,"&About (simple)...\tF1") {
+					WX::about_box(@frame,aboutinfo_minimal)
+				}
+				help.append_normal(:about_fancy,"About (&fancy)...\tShift-F1") {
+					WX::about_box(@frame,aboutinfo_website)
+				}
+				help.append_normal(:about_full,"About (f&ull)...\tCtrl-F1") {
+          WX::about_box(@frame,aboutinfo_all)
+				}
+				help.append_normal(:about_custom,"About (&custom)...\tCtrl-Shift-F1") {
+				  about = MyAboutDialog.new(@frame,aboutinfo_all)
+				  about.show_modal
+				}
+				
 			}
 		}
 		
