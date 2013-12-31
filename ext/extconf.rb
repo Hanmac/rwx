f4bcd905299652dd4d10e45284c8e6fe97a9d70c
Hanmac <hanmac@gmx.de>
2013-12-31 12:14:54 +0100

    ruby2.2trunk does have $CXXFLAGS in mkmf, use if possible

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,24 +5,25 @@ dir_config "rwx"
 
 if(wx_config = find_executable('wx-config'))
 
+  $CXXFLAGS = CONFIG["CXXFLAGS"] unless defined?($CXXFLAGS)
 	
 	case `#{wx_config} --basename`
 	when /gtk2/
-		CONFIG["CXXFLAGS"] << pkg_config("gtk+-x11-2.0")[0]
-		CONFIG["CXXFLAGS"] << pkg_config("gdk-x11-2.0")[0]
+    $CXXFLAGS << pkg_config("gtk+-x11-2.0")[0]
+    $CXXFLAGS << pkg_config("gdk-x11-2.0")[0]
 	when /gtk3/
-		CONFIG["CXXFLAGS"] << pkg_config("gtk+-x11-3.0")[0]
-		CONFIG["CXXFLAGS"] << pkg_config("gdk-x11-3.0")[0]
+	  $CXXFLAGS << pkg_config("gtk+-x11-3.0")[0]
+    $CXXFLAGS << pkg_config("gdk-x11-3.0")[0]
 	end
 
 	$CFLAGS << " -fvisibility-inlines-hidden -x c++ -g -Wall "
-	CONFIG["CXXFLAGS"] << " -fvisibility-inlines-hidden -g -Wall "
+  $CXXFLAGS << " -fvisibility-inlines-hidden -g -Wall "
 	$CPPFLAGS << " -fvisibility-inlines-hidden -g "
 	$LDFLAGS << " -fvisibility-inlines-hidden "
 
 
 	$CFLAGS << `#{wx_config} --cflags`.chomp
-	CONFIG["CXXFLAGS"] << `#{wx_config} --cxxflags`.chomp
+  $CXXFLAGS << `#{wx_config} --cxxflags`.chomp
 	$CPPFLAGS << `#{wx_config} --cppflags`.chomp
 	$LDFLAGS << `#{wx_config} --libs all`.chomp
 
