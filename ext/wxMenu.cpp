7dd4a18ddb583a872a395ebe0a1155af356e3533
Hanmac <hanmac@gmx.de>
2014-01-12 12:07:15 +0100

    wxMenu: make the _menu methods extra from the _normal methods. add some checks that the title may not be empty and raise exceptions if that happens. document all (add|insert|prepend)_(normal|check|radio|menu) methods

--- ext/wxMenu.cpp
+++ ext/wxMenu.cpp
@@ -77,54 +77,136 @@ void bind_callback(wxMenu* menu,wxWindowID id)
 	}
 }
 
+DLL_LOCAL VALUE _append_base(int argc,VALUE *argv,VALUE self,wxItemKind kind)
+{
+	VALUE id,text,help;
 
+	rb_scan_args(argc, argv, "12",&id,&text,&help);
+	wxWindowID wid(unwrapID(id));
+
+	if(!wxIsStockID(wid) && (NIL_P(text) || rb_str_length(text) == INT2FIX(0)))
+		rb_raise(rb_eArgError,"id %s (%d) needs an text",unwrap<char*>(id),wid);
+
+	wxMenuItem *item = _self->Append(wid,unwrap<wxString>(text),unwrap<wxString>(help),kind);
+	bind_callback(_self,item->GetId());
+	return wrap(item);
+}
+
+
+/*
+ * call-seq:
+ *   add_normal(id, text, [help]) -> WX::Menu::Item
+ *   add_normal(id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * adds a new normal menu item to the Menu widget.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
 DLL_LOCAL VALUE _appendNormalItem(int argc,VALUE *argv,VALUE self)
 {
-	VALUE id,text,help,temp;
+	return _append_base(argc,argv,self,wxITEM_NORMAL);
+}
 
-	rb_scan_args(argc, argv, "1*",&id,&temp);
+/*
+ * call-seq:
+ *   add_check(id, text, [help]) -> WX::Menu::Item
+ *   add_check(id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * adds a new check menu item to the Menu widget.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
+DLL_LOCAL VALUE _appendCheckItem(int argc,VALUE *argv,VALUE self)
+{
+	return _append_base(argc,argv,self,wxITEM_CHECK);
+}
 
-	if(rb_obj_is_kind_of(id,rb_cString) && rb_block_given_p()){
+/*
+ * call-seq:
+ *   add_radio(id, text, [help]) -> WX::Menu::Item
+ *   add_radio(id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * adds a new radio menu item to the Menu widget.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
+DLL_LOCAL VALUE _appendRadioItem(int argc,VALUE *argv,VALUE self)
+{
+	return _append_base(argc,argv,self,wxITEM_RADIO);
+}
+
+/*
+ * call-seq:
+ *   add_menu(text, [help]) {|menu| ... } -> WX::Menu::Item
+ *   add_menu(menu, text, [help]) -> WX::Menu::Item
+ *
+ * adds a new sub menu item to the Menu widget.
+ * when block is given, it created a new menu and yields it into the block.
+ * ===Arguments
+ * * text is the Label of the menu item. String
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
+DLL_LOCAL VALUE _append_menu(int argc,VALUE *argv,VALUE self)
+{
+	VALUE text,help,menu;
+
+	wxMenu *m = NULL;
+	if(rb_block_given_p()){
 		rb_scan_args(argc, argv, "11",&text,&help);
 		wxMenu *m = new wxMenu;
 		rb_yield(wrap(m));
-		return wrap(_self->AppendSubMenu(m,unwrap<wxString>(text),unwrap<wxString>(help)));
 	}else{
-		rb_scan_args(argc, argv, "12",&id,&text,&help);
-		wxWindowID wid = unwrapID(id);
-		if(!wxIsStockID(wid) && NIL_P(text))
-			rb_raise(rb_eArgError,"id %d needs an text",wid);
-		wxMenuItem *item = _self->Append(wid,unwrap<wxString>(text),unwrap<wxString>(help));
-		bind_callback(_self,item->GetId());
-		return wrap(item);
+		rb_scan_args(argc, argv, "12",&menu,&text,&help);
+		wxMenu *m = unwrap<wxMenu*>(menu);
+
+		if(!m)
+			rb_raise(rb_eTypeError,"menu can't be nil.");
 	}
-}
 
+	wxString wtext(unwrap<wxString>(text));
 
-DLL_LOCAL VALUE _appendCheckItem(int argc,VALUE *argv,VALUE self)
-{
-	VALUE id,text,help;
-	rb_scan_args(argc, argv, "12",&id,&text,&help);
-	wxMenuItem *item = _self->AppendCheckItem(unwrapID(id),unwrap<wxString>(text),unwrap<wxString>(help));
-	bind_callback(_self,item->GetId());
-	return wrap(item);
-}
+	if(wtext.empty())
+	{
+		if(m->GetTitle().empty())
+			rb_raise(rb_eArgError,"menu must have a title");
 
-DLL_LOCAL VALUE _appendRadioItem(int argc,VALUE *argv,VALUE self)
-{
-	VALUE id,text,help;
-	rb_scan_args(argc, argv, "12",&id,&text,&help);
-	wxMenuItem *item = _self->AppendRadioItem(unwrapID(id),unwrap<wxString>(text),unwrap<wxString>(help));
-	bind_callback(_self,item->GetId());
-	return wrap(item);
+		wtext = m->GetTitle();
+	}
+	return wrap(_self->AppendSubMenu(m,wtext,unwrap<wxString>(help)));
+
 }
 
 DLL_LOCAL VALUE _appendShift(VALUE self,VALUE val)
 {
-	wxWindowID id = unwrapID(val);
-	if(!wxIsStockID(id))
-		rb_raise(rb_eArgError,"id \"%s\" cant be fast added",unwrap<char*>(val));
-	_self->Append(id);
+	if(rb_obj_is_kind_of(val,rb_cWXMenuItem)) {
+		_self->Append(unwrap<wxMenuItem*>(val));
+	} else {
+		wxWindowID id = unwrapID(val);
+		if(!wxIsStockID(id))
+			rb_raise(rb_eArgError,"id \"%s\" cant be fast added",unwrap<char*>(val));
+		_self->Append(id);
+	}
 	return self;
 }
 
@@ -134,26 +216,124 @@ DLL_LOCAL VALUE _insert_base(int argc,VALUE *argv,VALUE self,wxItemKind kind)
 {
 	VALUE idx,id,text,help;
 	rb_scan_args(argc, argv, "22",&idx,&id,&text,&help);
-	wxMenuItem *item = _self->Insert(NUM2INT(idx),unwrapID(id),unwrap<wxString>(text),unwrap<wxString>(help),kind);
+
+	wxWindowID wid(unwrapID(id));
+
+	if(!wxIsStockID(wid) && (NIL_P(text) || rb_str_length(text) == INT2FIX(0)))
+		rb_raise(rb_eArgError,"id %s (%d) needs an text",unwrap<char*>(id),wid);
+
+	wxMenuItem *item = _self->Insert(NUM2INT(idx),wid,unwrap<wxString>(text),unwrap<wxString>(help),kind);
 	bind_callback(_self,item->GetId());
 	return wrap(item);
 }
 
 
+/*
+ * call-seq:
+ *   insert_normal(pos, id, text, [help]) -> WX::Menu::Item
+ *   insert_normal(pos, id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * inserts a new normal menu item to the Menu widget to the given position.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * pos where the item should be added.
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
 DLL_LOCAL VALUE _insertNormalItem(int argc,VALUE *argv,VALUE self)
 {
 	return _insert_base(argc,argv,self,wxITEM_NORMAL);
 }
 
-
+/*
+ * call-seq:
+ *   insert_check(pos, id, text, [help]) -> WX::Menu::Item
+ *   insert_check(pos, id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * inserts a new check menu item to the Menu widget to the given position.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * pos where the item should be added.
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
 DLL_LOCAL VALUE _insertCheckItem(int argc,VALUE *argv,VALUE self)
 {
 	return _insert_base(argc,argv,self,wxITEM_CHECK);
 }
 
+/*
+ * call-seq:
+ *   insert_radio(pos, id, text, [help]) -> WX::Menu::Item
+ *   insert_radio(pos, id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * inserts a new radio menu item to the Menu widget to the given position.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * pos where the item should be added.
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
 DLL_LOCAL VALUE _insertRadioItem(int argc,VALUE *argv,VALUE self)
 {
 	return _insert_base(argc,argv,self,wxITEM_RADIO);
+}
+
+/*
+ * call-seq:
+ *   insert_menu(pos, text, [help]) {|menu| ... } -> WX::Menu::Item
+ *   insert_menu(pos, menu, text, [help]) -> WX::Menu::Item
+ *
+ * insert a new sub menu item to the Menu widget into the given position.
+ * when block is given, it created a new menu and yields it into the block.
+ * ===Arguments
+ * * pos where the item should be added.
+ * * text is the Label of the menu item. String
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
+DLL_LOCAL VALUE _insert_menu(int argc,VALUE *argv,VALUE self)
+{
+	VALUE idx,text,help,menu;
+
+	wxMenu *m = NULL;
+	if(rb_block_given_p()){
+		rb_scan_args(argc, argv, "21",&idx,&text,&help);
+		wxMenu *m = new wxMenu;
+		rb_yield(wrap(m));
+	}else{
+		rb_scan_args(argc, argv, "22",&idx,&menu,&text,&help);
+		wxMenu *m = unwrap<wxMenu*>(menu);
+
+		if(!m)
+			rb_raise(rb_eTypeError,"menu can't be nil.");
+	}
+
+	wxString wtext(unwrap<wxString>(text));
+
+	if(wtext.empty())
+	{
+		if(m->GetTitle().empty())
+			rb_raise(rb_eArgError,"menu must have a title");
+
+		wtext = m->GetTitle();
+	}
+	return wrap(_self->Insert(NUM2UINT(idx),wxID_ANY,wtext,m,unwrap<wxString>(help)));
+
 }
 
 
@@ -161,27 +341,122 @@ DLL_LOCAL VALUE _prepend_base(int argc,VALUE *argv,VALUE self,wxItemKind kind)
 {
 	VALUE id,text,help;
 	rb_scan_args(argc, argv, "12",&id,&text,&help);
-	wxMenuItem *item = _self->Prepend(unwrapID(id),unwrap<wxString>(text),unwrap<wxString>(help),kind);
+
+	wxWindowID wid(unwrapID(id));
+
+	if(!wxIsStockID(wid) && (NIL_P(text) || rb_str_length(text) == INT2FIX(0)))
+		rb_raise(rb_eArgError,"id %s (%d) needs an text",unwrap<char*>(id),wid);
+
+
+	wxMenuItem *item = _self->Prepend(wid,unwrap<wxString>(text),unwrap<wxString>(help),kind);
 	bind_callback(_self,item->GetId());
 	return wrap(item);
 }
 
-
+/*
+ * call-seq:
+ *   prepend_normal(id, text, [help]) -> WX::Menu::Item
+ *   prepend_normal(id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * prepends a new normal menu item to the Menu widget.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
 DLL_LOCAL VALUE _prependNormalItem(int argc,VALUE *argv,VALUE self)
 {
 	return _prepend_base(argc,argv,self,wxITEM_NORMAL);
 }
 
-
+/*
+ * call-seq:
+ *   prepend_check(id, text, [help]) -> WX::Menu::Item
+ *   prepend_check(id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * prepends a new check menu item to the Menu widget.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
 DLL_LOCAL VALUE _prependCheckItem(int argc,VALUE *argv,VALUE self)
 {
 	return _prepend_base(argc,argv,self,wxITEM_CHECK);
 }
 
+/*
+ * call-seq:
+ *   prepend_radio(id, text, [help]) -> WX::Menu::Item
+ *   prepend_radio(id, text, [help]) {|event| ... } -> WX::Menu::Item
+ *
+ * prepends a new radio menu item to the Menu widget.
+ * when block is given, bind the block to the event of the menu item.
+ * ===Arguments
+ * * id of the menu item: Symbol/Integer/nil
+ * * text is the Label of the menu item. String (can be nil if id is one of the pre defined)
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
 DLL_LOCAL VALUE _prependRadioItem(int argc,VALUE *argv,VALUE self)
 {
 	return _prepend_base(argc,argv,self,wxITEM_RADIO);
 }
+
+/*
+ * call-seq:
+ *   prepend_menu(text, [help]) {|menu| ... } -> WX::Menu::Item
+ *   prepend_menu(menu, text, [help]) -> WX::Menu::Item
+ *
+ * prepends a new sub menu item to the Menu widget.
+ * when block is given, it created a new menu and yields it into the block.
+ * ===Arguments
+ * * text is the Label of the menu item. String
+ * * help shown in WX::StatusBar. String
+ * ===Return value
+ * WX::Menu::Item
+ *
+*/
+DLL_LOCAL VALUE _prepend_menu(int argc,VALUE *argv,VALUE self)
+{
+	VALUE text,help,menu;
+
+	wxMenu *m = NULL;
+	if(rb_block_given_p()){
+		rb_scan_args(argc, argv, "11",&text,&help);
+		wxMenu *m = new wxMenu;
+		rb_yield(wrap(m));
+	}else{
+		rb_scan_args(argc, argv, "12",&menu,&text,&help);
+		wxMenu *m = unwrap<wxMenu*>(menu);
+
+		if(!m)
+			rb_raise(rb_eTypeError,"menu can't be nil.");
+	}
+
+	wxString wtext(unwrap<wxString>(text));
+
+	if(wtext.empty())
+	{
+		if(m->GetTitle().empty())
+			rb_raise(rb_eArgError,"menu must have a title");
+
+		wtext = m->GetTitle();
+	}
+	return wrap(_self->Prepend(wxID_ANY,wtext,m,unwrap<wxString>(help)));
+
+}
+
 
 #if wxUSE_XRC
 DLL_LOCAL VALUE _load_xrc(VALUE self,VALUE name)
@@ -218,19 +493,23 @@ DLL_LOCAL void Init_WXMenu(VALUE rb_mWX)
 	rb_define_attr_method(rb_cWXMenu,"parent",_getParent,_setParent);
 
 	rb_define_method(rb_cWXMenu,"each",RUBY_METHOD_FUNC(_each),0);
+
 	rb_define_method(rb_cWXMenu,"append_normal",RUBY_METHOD_FUNC(_appendNormalItem),-1);
 	rb_define_method(rb_cWXMenu,"append_check",RUBY_METHOD_FUNC(_appendCheckItem),-1);
 	rb_define_method(rb_cWXMenu,"append_radio",RUBY_METHOD_FUNC(_appendRadioItem),-1);
+	rb_define_method(rb_cWXMenu,"append_menu",RUBY_METHOD_FUNC(_append_menu),-1);
 
 	rb_define_method(rb_cWXMenu,"append_separator",RUBY_METHOD_FUNC(_AppendSeparator),0);
 
 	rb_define_method(rb_cWXMenu,"insert_normal",RUBY_METHOD_FUNC(_insertNormalItem),-1);
 	rb_define_method(rb_cWXMenu,"insert_check",RUBY_METHOD_FUNC(_insertCheckItem),-1);
 	rb_define_method(rb_cWXMenu,"insert_radio",RUBY_METHOD_FUNC(_insertRadioItem),-1);
+	rb_define_method(rb_cWXMenu,"insert_menu",RUBY_METHOD_FUNC(_insert_menu),-1);
 
 	rb_define_method(rb_cWXMenu,"prepend_normal",RUBY_METHOD_FUNC(_prependNormalItem),-1);
 	rb_define_method(rb_cWXMenu,"prepend_check",RUBY_METHOD_FUNC(_prependCheckItem),-1);
 	rb_define_method(rb_cWXMenu,"prepend_radio",RUBY_METHOD_FUNC(_prependRadioItem),-1);
+	rb_define_method(rb_cWXMenu,"prepend_menu",RUBY_METHOD_FUNC(_prepend_menu),-1);
 
 	rb_define_method(rb_cWXMenu,"prepend_separator",RUBY_METHOD_FUNC(_PrependSeparator),0);
 
