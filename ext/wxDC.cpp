f4709eb664c29e017f9f94ddc7f217575f5c5e63
Hanmac <hanmac@gmx.de>
2014-04-18 15:07:26 +0200

    wxDC: add more properties and document them

--- ext/wxDC.cpp
+++ ext/wxDC.cpp
@@ -31,22 +31,60 @@ macro_attr(TextBackground,wxColour)
 
 
 singlefunc(Clear)
+
+singlereturn(GetSize)
+
+singlereturn(CanDrawBitmap)
+
 singlereturn(GetLogicalOrigin)
 singlereturn(GetDeviceOrigin)
 
 DLL_LOCAL VALUE _setLogicalOrigin(VALUE self,VALUE val)
 {
 	rb_check_frozen(self);
-	wxPoint *p = unwrap<wxPoint*>(val);
-	_self->SetLogicalOrigin(p->x,p->y);
+	wxPoint p(unwrap<wxPoint>(val));
+	_self->SetLogicalOrigin(p.x,p.y);
 	return val;
 }
 
 DLL_LOCAL VALUE _setDeviceOrigin(VALUE self,VALUE val)
 {
 	rb_check_frozen(self);
-	wxPoint *p = unwrap<wxPoint*>(val);
-	_self->SetDeviceOrigin(p->x,p->y);
+	wxPoint p(unwrap<wxPoint>(val));
+	_self->SetDeviceOrigin(p.x,p.y);
+	return val;
+}
+
+
+DLL_LOCAL VALUE _getUserScale(VALUE self)
+{
+	rb_check_frozen(self);
+	double x, y;
+	_self->GetUserScale(&x,&y);
+	return wrap(wxRealPoint(x,y));
+}
+
+DLL_LOCAL VALUE _setUserScale(VALUE self,VALUE val)
+{
+	rb_check_frozen(self);
+	wxRealPoint p(unwrap<wxRealPoint>(val));
+	_self->SetUserScale(p.x,p.y);
+	return val;
+}
+
+DLL_LOCAL VALUE _getLogicalScale(VALUE self)
+{
+	rb_check_frozen(self);
+	double x, y;
+	_self->GetLogicalScale(&x,&y);
+	return wrap(wxRealPoint(x,y));
+}
+
+DLL_LOCAL VALUE _setLogicalScale(VALUE self,VALUE val)
+{
+	rb_check_frozen(self);
+	wxRealPoint p(unwrap<wxRealPoint>(val));
+	_self->SetLogicalScale(p.x,p.y);
 	return val;
 }
 
@@ -90,6 +128,8 @@ DLL_LOCAL VALUE _DrawCheckMark(VALUE self,VALUE rect)
 DLL_LOCAL VALUE _DrawBitmap(VALUE self,VALUE bitmap,VALUE x,VALUE y)
 {
 
+	if(!_self->CanDrawBitmap())
+		rb_raise(rb_eArgError, "DC can't draw bitmaps");
 	_self->DrawBitmap(unwrap<wxBitmap>(bitmap),NUM2INT(x),NUM2INT(y));
 	return self;
 }
@@ -114,16 +154,63 @@ DLL_LOCAL VALUE _DrawLines(int argc,VALUE *argv,VALUE self)
 }
 }
 
+
+/* Document-attr: font
+ * WX::Font for drawing text
+ */
+
+/* Document-attr: pen
+ * WX::Pen
+ */
+
+/* Document-attr: brush
+ * WX::Brush for lines
+ */
+
+/* Document-attr: background
+ * WX::Brush for filling
+ */
+
+/* Document-attr: text_foreground
+ * WX::Color for text drawing.
+ */
+
+/* Document-attr: text_background
+ * WX::Color for text drawing.
+ */
+
+
+/* Document-attr: logical_origin
+ * WX::Point
+ */
+
+/* Document-attr: device_origin
+ * WX::Point the device origin (i.e. the origin in pixels after scaling has been applied).
+ */
+
+/* Document-attr: user_scale
+ * WX::Point (Float,Float) user scaling factor, useful for applications which require 'zooming'.
+ */
+
+/* Document-attr: logical_scale
+ * WX::Point
+ */
+
+
 DLL_LOCAL void Init_WXDC(VALUE rb_mWX)
 {
 #if 0
-	rb_define_attr_method(rb_cWXDC,"font",_getFont,_setFont);
-	rb_define_attr_method(rb_cWXDC,"pen",_getPen,_setPen);
-	rb_define_attr_method(rb_cWXDC,"brush",_getBrush,_setBrush);
-	rb_define_attr_method(rb_cWXDC,"background",_getBackground,_setBackground);
-	rb_define_attr_method(rb_cWXDC,"text_foreground",_getTextForeground,_setTextForeground);
-	rb_define_attr_method(rb_cWXDC,"text_background",_getTextBackground,_setTextBackground);
+	rb_define_attr(rb_cWXDC,"font",1,1);
+	rb_define_attr(rb_cWXDC,"pen",1,1);
+	rb_define_attr(rb_cWXDC,"brush",1,1);
+	rb_define_attr(rb_cWXDC,"background",1,1);
+	rb_define_attr(rb_cWXDC,"text_foreground",1,1);
+	rb_define_attr(rb_cWXDC,"text_background",1,1);
 
+	rb_define_attr(rb_cWXDC,"logical_origin",1,1);
+	rb_define_attr(rb_cWXDC,"device_origin",1,1);
+	rb_define_attr(rb_cWXDC,"user_scale",1,1);
+	rb_define_attr(rb_cWXDC,"logical_scale",1,1);
 #endif
 
 	using namespace RubyWX::DC;
@@ -142,6 +229,8 @@ DLL_LOCAL void Init_WXDC(VALUE rb_mWX)
 	rb_define_method(rb_cWXDC,"draw_text",RUBY_METHOD_FUNC(_DrawText),3);
 
 	rb_define_method(rb_cWXDC,"clear",RUBY_METHOD_FUNC(_Clear),0);
+	rb_define_method(rb_cWXDC,"size",RUBY_METHOD_FUNC(_GetSize),0);
+	rb_define_method(rb_cWXDC,"draw_bitmap?",RUBY_METHOD_FUNC(_CanDrawBitmap),0);
 
 	rb_define_attr_method(rb_cWXDC,"font",_getFont,_setFont);
 	rb_define_attr_method(rb_cWXDC,"pen",_getPen,_setPen);
@@ -149,6 +238,13 @@ DLL_LOCAL void Init_WXDC(VALUE rb_mWX)
 	rb_define_attr_method(rb_cWXDC,"background",_getBackground,_setBackground);
 	rb_define_attr_method(rb_cWXDC,"text_foreground",_getTextForeground,_setTextForeground);
 	rb_define_attr_method(rb_cWXDC,"text_background",_getTextBackground,_setTextBackground);
+
+	rb_define_attr_method(rb_cWXDC,"logical_origin",_GetLogicalOrigin,_setLogicalOrigin);
+	rb_define_attr_method(rb_cWXDC,"device_origin",_GetDeviceOrigin,_setDeviceOrigin);
+
+	rb_define_attr_method(rb_cWXDC,"user_scale",_getUserScale,_setUserScale);
+	rb_define_attr_method(rb_cWXDC,"logical_scale",_getLogicalScale,_setLogicalScale);
+
 
 	registerInfo<wxDC>(rb_cWXDC);
 }
