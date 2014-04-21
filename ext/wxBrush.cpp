47a026387fb4e4684593528259d0985358b5ec16
Hanmac <hanmac@gmx.de>
2014-04-21 13:42:00 +0200

    wxBrush: use wxTheBrushList to add singleton function, add and document predefined brush constants

--- ext/wxBrush.cpp
+++ ext/wxBrush.cpp
@@ -13,6 +13,11 @@
 
 VALUE rb_cWXBrush;
 
+
+typedef std::map<wxBrush*,VALUE> brushlisttype;
+brushlisttype brushlistholder;
+
+
 template <>
 wxBrush* nullPtr<wxBrush*>(){ return &wxNullBrush;}
 template <>
@@ -108,8 +113,91 @@ DLL_LOCAL VALUE _equal(VALUE self, VALUE other)
 }
 
 
+DLL_LOCAL VALUE _class_get(int argc,VALUE *argv,VALUE self)
+{
+
+	VALUE color,style;
+	rb_scan_args(argc, argv, "11",&color,&style);
+	//TODO add refcounting
+	wxBrush *brush = wxTheBrushList->FindOrCreateBrush(
+		unwrap<wxColor>(color),
+		NIL_P(style) ? wxBRUSHSTYLE_SOLID : unwrapenum<wxBrushStyle>(style)
+	);
+
+	if(!brush || !brush->IsOk())
+		return Qnil;
+
+	//look in the PenList holder is brush was already wrapped
+	brushlisttype::iterator it = brushlistholder.find(brush);
+	if(it != penlistholder.end()) {
+		return it->second;
+	} else {
+		//wrap wxBrush pointer to ruby object
+		VALUE result = wrap(brush);
+
+		//BrushList objects should not be changed
+		rb_obj_freeze(result);
+		//Prevent BrushList objects from been deleted
+		rwx_refobject(result);
+		//add wrapped brush to the BrushList holder to find it again
+		brushlistholder[brush] = result;
+		return result;
+	}
+}
+
 }
 }
+
+/*
+ * Document-class: WX::Brush
+ *
+ * This class represents an Brush.
+*/
+
+
+/* Document-attr: color
+ * returns the color of the Brush. WX::Color
+ */
+/* Document-attr: style
+ * returns the style of the Brush. Symbol
+ */
+/* Document-attr: stipple
+ * returns the stipple of the Brush. WX::Bitmap
+ */
+
+/* Document-const: BLACK
+ * predefined Brush constant.
+ */
+/* Document-const: BLUE
+ * predefined Brush constant.
+ */
+/* Document-const: CYAN
+ * predefined Brush constant.
+ */
+/* Document-const: GREEN
+ * predefined Brush constant.
+ */
+/* Document-const: YELLOW
+ * predefined Brush constant.
+ */
+/* Document-const: GREY
+ * predefined Brush constant.
+ */
+/* Document-const: LIGHT_GREY
+ * predefined Brush constant.
+ */
+/* Document-const: MEDIUM_GREY
+ * predefined Brush constant.
+ */
+/* Document-const: RED
+ * predefined Brush constant.
+ */
+/* Document-const: TRANSPARENT
+ * predefined Brush constant.
+ */
+/* Document-const: WHITE
+ * predefined Brush constant.
+ */
 
 DLL_LOCAL void Init_WXBrush(VALUE rb_mWX)
 {
@@ -142,6 +230,19 @@ DLL_LOCAL void Init_WXBrush(VALUE rb_mWX)
 	//
 //	rb_define_method(rb_cWXBrush,"to_s",RUBY_METHOD_FUNC(_tos),0);
 //	rb_define_method(rb_cWXBrush,"inspect",RUBY_METHOD_FUNC(_inspect),0);
+
+
+	rb_define_const(rb_cWXBrush,"BLACK",wrap(wxBLACK_BRUSH));
+	rb_define_const(rb_cWXBrush,"BLUE",wrap(wxBLUE_BRUSH));
+	rb_define_const(rb_cWXBrush,"CYAN",wrap(wxCYAN_BRUSH));
+	rb_define_const(rb_cWXBrush,"GREEN",wrap(wxGREEN_BRUSH));
+	rb_define_const(rb_cWXBrush,"YELLOW",wrap(wxYELLOW_BRUSH));
+	rb_define_const(rb_cWXBrush,"GREY",wrap(wxGREY_BRUSH));
+	rb_define_const(rb_cWXBrush,"LIGHT_GREY",wrap(wxLIGHT_GREY_BRUSH));
+	rb_define_const(rb_cWXBrush,"MEDIUM_GREY",wrap(wxMEDIUM_GREY_BRUSH));
+	rb_define_const(rb_cWXBrush,"RED",wrap(wxRED_BRUSH));
+	rb_define_const(rb_cWXBrush,"TRANSPARENT",wrap(wxTRANSPARENT_BRUSH));
+	rb_define_const(rb_cWXBrush,"WHITE",wrap(wxWHITE_BRUSH));
 
 	registerInfo<wxBrush>(rb_cWXBrush);
 	registerEnum<wxBrushStyle>("WX::BrushStyle",wxBRUSHSTYLE_TRANSPARENT)
