f75283cfbfead7a90805006347d9711c67465a80
Hanmac <hanmac@gmx.de>
2014-05-04 16:37:14 +0200

    wxCheckListBox: uses Listbox style flags, document each_checked, and add and document (get|set)_item_checked

--- ext/wxCheckListBox.cpp
+++ ext/wxCheckListBox.cpp
@@ -39,12 +39,15 @@ DLL_LOCAL VALUE _initialize(int argc,VALUE *argv,VALUE self)
 		wxArrayString choices;
 		int style(0);
 		int selection(-1);
+		bool selflag(false);
 
 		if(rb_obj_is_kind_of(hash,rb_cHash)) {
 			set_hash_option(hash,"id",id,unwrapID);
 			set_hash_option(hash,"choices",choices);
 			set_hash_option(hash,"style",style);
-			set_hash_option(hash,"selection",selection);
+			selflag = set_hash_option(hash,"selection",selection);
+
+			ListBox::set_style_flags(hash,style);
 		}
 		_self->Create(
 			unwrap<wxWindow*>(parent),id,
@@ -52,7 +55,8 @@ DLL_LOCAL VALUE _initialize(int argc,VALUE *argv,VALUE self)
 			choices,style
 		);
 		
-		_self->SetSelection(selection);
+		if(selflag && check_index(selection,_self->GetCount()))
+			_self->SetSelection(selection);
 
 	}
 	rb_call_super(argc,argv);
@@ -67,6 +71,16 @@ DLL_LOCAL VALUE _each_checked_size(VALUE self)
 }
 
 
+/*
+ * call-seq:
+ *   each_checked -> Enumerator
+ *   each_checked { |idx, text| } -> self
+ *
+ * iterates the checked items of the list box.
+ * ===Return value
+ * self
+ *
+*/
 DLL_LOCAL VALUE _each_checked(VALUE self)
 {
 	RETURN_SIZED_ENUMERATOR(self,0,NULL,RUBY_METHOD_FUNC(_each_checked_size));
@@ -87,6 +101,55 @@ DLL_LOCAL VALUE _getCheckedItems(VALUE self)
 	_self->GetCheckedItems(data);
 	return wrap(data);
 }
+
+/*
+ * call-seq:
+ *   get_item_checked(pos) -> true/false
+ *
+ * Determines whether an item is checked.
+ * ===Arguments
+ * * pos of the item. Integer
+ * ===Return value
+ * true/false
+ * === Exceptions
+ * [IndexError]
+ * * pos is greater than the count of items
+ *
+*/
+DLL_LOCAL VALUE _getItemChecked(VALUE self,VALUE idx)
+{
+	int cidx = NUM2INT(idx);
+	if(check_index(cidx,_self->GetCount()))
+		return wrap(_self->IsChecked(cidx));
+	return Qnil;
+}
+
+/*
+ * call-seq:
+ *   set_item_selection(pos,val) -> self
+ *
+ * sets the item at the position as checked if val is true.
+ * ===Arguments
+ * * pos of the item. Integer
+ * * val if item should be checked. true/false
+ * ===Return value
+ * self
+ * === Exceptions
+ * [IndexError]
+ * * pos is greater than the count of items
+ *
+*/
+DLL_LOCAL VALUE _setItemChecked(VALUE self,VALUE idx,VALUE val)
+{
+	rb_check_frozen(self);
+
+	int cidx = NUM2INT(idx);
+	if(check_index(cidx,_self->GetCount()))
+		_self->Check(cidx,RTEST(val));
+
+	return self;
+}
+
 
 
 }
@@ -113,6 +176,9 @@ DLL_LOCAL void Init_WXCheckListBox(VALUE rb_mWX)
 
 	rb_define_method(rb_cWXCheckListBox,"each_checked",RUBY_METHOD_FUNC(_each_checked),0);
 	rb_define_method(rb_cWXCheckListBox,"checked_items",RUBY_METHOD_FUNC(_getCheckedItems),0);
+
+	rb_define_method(rb_cWXListBox,"get_item_checked",RUBY_METHOD_FUNC(_getItemChecked),1);
+	rb_define_method(rb_cWXListBox,"set_item_checked",RUBY_METHOD_FUNC(_setItemChecked),2);
 
 
 	registerEventType("checklistbox", wxEVT_CHECKLISTBOX,rb_cWXCommandEvent);
