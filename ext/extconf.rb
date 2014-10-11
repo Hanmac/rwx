b723d8f9c17d90383614612546a7686a087a7a21
Hanmac <hanmac@gmx.de>
2014-10-11 20:21:06 +0200

    add compiler checks into extconf.rb, rwx does need to be compiled with the same compiler as ruby and wxWidgets, and so ruby and wxWidgets does need to be build with the same compiler

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -10,7 +10,30 @@ end
 dir_config "rwx"
 
 if(wx_config = find_executable('wx-config'))
-
+	
+	if `#{wx_config} --version`.chomp < "3.0.0"
+		abort("wx version outdated, please update to 3.0.0 or newer")
+	end
+	
+	# An ruby extension does need to be build against
+	# the same compiler as ruby was
+	unless CONFIG["CC"] && find_executable(CONFIG["CC"])
+		abort("C compiler not found!")
+	end
+	unless CONFIG["CXX"] && find_executable(CONFIG["CXX"])
+		abort("C++ compiler not found!")
+	end
+	
+	cc = `#{wx_config} --cc`.chomp
+	unless cc == CONFIG["CC"]
+		warn("CC compiler missmatch %s == %s" % [cc, CONFIG["CC"]])
+	end
+	
+	cxx = `#{wx_config} --cxx`.chomp
+	unless cxx == CONFIG["CXX"]
+		warn("CXX compiler missmatch %s == %s" % [cxx, CONFIG["CXX"]])
+	end
+	
 	#earlier versions of ruby does not have that constant
 	$CXXFLAGS = CONFIG["CXXFLAGS"] unless defined?($CXXFLAGS)
 	
@@ -28,32 +51,29 @@ if(wx_config = find_executable('wx-config'))
 		#because pkg forgot to add the include paths to cxx flags
 		$CXXFLAGS << pkg[0] if pkg && !$CXXFLAGS[pkg[0]]
 	}
-
+	
 	all = " -fvisibility-inlines-hidden"
 	$CFLAGS << all << " -x c++ -g -Wall "
 	$CXXFLAGS << all << " -g -Wall "
 	$CPPFLAGS << all << " -g "
 	$LDFLAGS << all << " "
-
+	
 	# add the wx-config flags
 	$CFLAGS << `#{wx_config} --cflags`.chomp
 	$CXXFLAGS << `#{wx_config} --cxxflags`.chomp
 	$CPPFLAGS << `#{wx_config} --cppflags`.chomp
 	$LDFLAGS << `#{wx_config} --libs all`.chomp
 	
-	# An ruby extension does need to be build against
-	# the same compiler as ruby was
-	unless CONFIG["CC"] && find_executable(CONFIG["CC"])
-		abort("C compiler not found!")
-	end
-	unless CONFIG["CXX"] && find_executable(CONFIG["CXX"])
-		abort("C++ compiler not found!")
-	end
-	
-	have_header("wx/preferences.h")
-	
 	with_cflags(" -x c++ ") {
+		# need c++ for some of the tests
 		RbConfig::CONFIG["CC"] = CONFIG["CXX"]
+		
+		have_header("wx/preferences.h")
+		
+		#check for better Bind commmand
+		unless have_macro("wxHAS_EVENT_BIND","wx/wx.h")
+			abort("need wxHAS_EVENT_BIND, update your compiler!")
+		end
 		
 		#check for Default-Constructors
 		have_func("wxContextHelpButton()","wx/cshelp.h")
@@ -70,12 +90,7 @@ if(wx_config = find_executable('wx-config'))
 		
 		#check for enum flags
 		have_const("wxFD_NO_FOLLOW","wx/filedlg.h")
-    have_const("wxDIRCTRL_DEFAULT_STYLE",["wx/wx.h", "wx/dirctrl.h"])
-		
-		#check for better Bind commmand
-		unless have_macro("wxHAS_EVENT_BIND","wx/wx.h")
-			abort("need wxHAS_EVENT_BIND, update your compiler!")
-		end
+		have_const("wxDIRCTRL_DEFAULT_STYLE",["wx/wx.h", "wx/dirctrl.h"])
 		
 	}
 else
