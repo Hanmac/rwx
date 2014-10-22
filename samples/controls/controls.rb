7ca941901d7c39741e77d6318d0c6015325fce00
Hans Mackowiak <hanmac@gmx.de>
2014-10-22 09:16:07 +0200

    Update controls.rb
    make general function for WX::ListBox and WX::Choice

--- samples/controls/controls.rb
+++ samples/controls/controls.rb
@@ -3,6 +3,50 @@ require "../../ext/rwx"
 
 class SamplesControlApp < WX::App
   
+  def create_list(panel, klass, choices)
+    normal = klass.new(panel, :choices => choices, :position => [10,10], :size => [120,70], :multiple => true)
+    sorted = klass.new(panel, :choices => choices[0,3], :position => [10,90], :size => [120,70], :sort => true)
+    both = [nornal,sorted]
+    
+    WX::Button.new(panel, :label => "Select #&2", :position => [180,30], :size => [140,30] ) {|button|
+      button.bind(:button) {
+        both.each { |list|
+          list.selection = 2 if list.item_count > 2
+        }
+      }
+    }
+
+    WX::Button.new(panel, :label => "&Select 'This'", :position => [340,30], :size => [140,30] ) {|button|
+      button.bind(:button) {
+        both.each { |list|
+          list.string_selection = "This" 
+        }
+      }
+    }
+    
+    WX::Button.new(panel, :label => "&Clear", :position => [180,80], :size => [140,30] ) {|button|
+      button.bind(:button) {
+        both.each { |list|
+          list.clear 
+        }
+      }
+    }
+
+    WX::Button.new(panel, :label => "&Append 'Hi!'", :position => [340,80], :size => [140,30] ) {|button|
+      button.bind(:button) {
+        both.each { |list|
+          list.append "Hi kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk!"
+        }
+      }
+    }
+    
+    WX::Button.new(panel, :label => "D&elete selected item", :position => [180,130], :size => [140,30] ) {|button|
+      button.bind(:button) {
+        yield normal, sorted
+      }
+    }
+  end
+  
   def on_init
   
     @frame = WX::Frame.new(nil,
@@ -36,90 +80,24 @@ class SamplesControlApp < WX::App
       @book.image_list = ["icons/list.xpm","icons/choice.xpm"]
         
       @book.add_page(WX::Panel,"wxListBox",true,0) {|panel|
-        listbox = WX::ListBox.new(panel, :choices => choices, :position => [10,10], :size => [120,70], :multiple => true)
-        listbox_sorted = WX::ListBox.new(panel, :choices => choices[0,3], :position => [10,90], :size => [120,70], :sort => true)
-        
-        WX::Button.new(panel, :label => "Select #&2", :position => [180,30], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            listbox.selection = 2 if listbox.item_count > 2
-            listbox_sorted.selection = 2 if listbox_sorted.item_count > 2
+        create_list(panel, WX::ListBox, choices) {|normal, sorted|
+          Array(normal.selection).reverse_each {|sel| 
+            normal.delete_item(sel) if sel != -1
           }
-        }
-
-        WX::Button.new(panel, :label => "&Select 'This'", :position => [340,30], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            listbox.string_selection = "This" 
-            listbox_sorted.string_selection = "This"
-          }
-        }
-        
-        WX::Button.new(panel, :label => "&Clear", :position => [180,80], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            listbox.clear 
-            listbox_sorted.clear
-          }
-        }
-  
-        WX::Button.new(panel, :label => "&Append 'Hi!'", :position => [340,80], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            listbox.append "Hi kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk!"
-            listbox_sorted.append "Hi hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh!"
-          }
-        }
-        
-        WX::Button.new(panel, :label => "D&elete selected item", :position => [180,130], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            Array(listbox.selection).reverse_each {|sel| 
-              listbox.delete_item(sel) if sel != -1
-            }
-            
-            sel = listbox_sorted.selection
-            listbox_sorted.delete_item(sel) if sel != -1
-          }
+          
+          sel = sorted.selection
+          sorted.delete_item(sel) if sel != -1
         }
       }
       
       
       @book.add_page(WX::Panel,"wxChoice",false,1) {|panel|
-        choice = WX::Choice.new(panel, :choices => choices, :position => [10,10])
-        choice_sorted = WX::Choice.new(panel, :choices => choices[0,3], :position => [10,90], :size => [120,-1], :sort => true)
-        
-        WX::Button.new(panel, :label => "Select #&2", :position => [180,30], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            choice.selection = 2 if choice.item_count > 2
-            choice_sorted.selection = 2 if choice_sorted.item_count > 2
-          }
-        }
-
-        WX::Button.new(panel, :label => "&Select 'This'", :position => [340,30], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            choice.string_selection = "This" if choice.items.include?("This") 
-              choice_sorted.string_selection = "This" if choice_sorted.items.include?("This")
-          }
-        }
-        
-        WX::Button.new(panel, :label => "&Clear", :position => [180,80], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            choice.clear 
-            choice_sorted.clear
-          }
-        }
-  
-        WX::Button.new(panel, :label => "&Append 'Hi!'", :position => [340,80], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            choice.append "Hi kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk!"
-            choice_sorted.append "Hi hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh!"
-          }
-        }
-        
-        WX::Button.new(panel, :label => "D&elete selected item", :position => [180,130], :size => [140,30] ) {|button|
-          button.bind(:button) {
-            sel = choice.selection 
-            choice.delete_item(sel) if sel != -1
-            
-            sel = choice_sorted.selection
-            choice_sorted.delete_item(sel) if sel != -1
-          }
+        create_list(panel, WX::Choice, choices) {|normal, sorted|
+          sel = normal.selection 
+          normal.delete_item(sel) if sel != -1
+          
+          sel = sorted.selection
+          sorted.delete_item(sel) if sel != -1
         }
       }
       
