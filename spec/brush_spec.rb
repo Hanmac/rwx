0c72ca5dda33d0f14c3cddd6337e6122eb8606ea
Hanmac <hanmac@gmx.de>
2014-10-26 21:05:07 +0100

    brush_spec: use shared copyable and dumpable, do the test again with image as stipple

--- spec/brush_spec.rb
+++ spec/brush_spec.rb
@@ -1,5 +1,19 @@
 RSpec.describe WX::Brush do
+  subject { WX::Brush::RED }
+    
+  include_examples "copyable"
+  include_examples "dumpable"
 
+  describe "with stipple" do
+    subject { 
+      b = WX::Brush::new(WX::Color::RED)
+      b.stipple = WX::Image.new(3, 4)
+      b
+    }
+    include_examples "copyable"
+    include_examples "dumpable"
+  end
+  
   include_examples "gdiobject"
 
 end
