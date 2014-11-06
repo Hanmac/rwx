6a9513e542d960d48cfcabda970966088f5fe679
Hanmac <hanmac@gmx.de>
2014-11-06 03:26:48 +0100

    wxTextCtrl: use set_obj_option

--- ext/wxTextCtrl.cpp
+++ ext/wxTextCtrl.cpp
@@ -87,8 +87,7 @@ DLL_LOCAL VALUE _initialize(int argc,VALUE *argv,VALUE self)
 	if(rb_obj_is_kind_of(name,rb_cString) &&
 		rb_obj_is_kind_of(hash,rb_cHash))
 	{
-		VALUE temp;
-		set_option(value,Value,wxString)
+		set_obj_option(hash,"value", &wxTextCtrl::SetValue, _self);
 	}
 
 	return self;
