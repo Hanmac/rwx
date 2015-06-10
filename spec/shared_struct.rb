acc0a0ce9909b4ea90ee4b8d896a54caaded324a
Hans Mackowiak <hanmac@gmx.de>
2015-06-10 15:20:07 +0200

    add shared spec for objects which are equal to objects with specific methods, or Arrays with specific length

---
+++ spec/shared_struct.rb
@@ -0,0 +1,11 @@
+require "ostruct"
+
+RSpec.shared_examples "struct_equal" do |data|
+  it "equal struct version" do
+    should eq(OpenStruct.new(data))
+  end
+  it "equal array version" do
+    should eq(data.values)
+    should_not eq(data.values * 2) #should not work with array of double size
+  end
+end
