021c0c5982cded77b403d98c9c38b53ddbb5ce2d
Hans Mackowiak <hanmac@gmx.de>
2015-06-12 16:26:47 +0200

    add new functions for something with text ctrl in widgets sample

--- samples/widgets/pages/common.rb
+++ samples/widgets/pages/common.rb
@@ -8,6 +8,27 @@ class CommonPage < WX::Panel
     create_content
   end
 
+  def with_text(ctrl, id = nil)
+    WX::BoxSizer.new {|siz|
+      siz.add(ctrl)
+      text = WX::TextCtrl.new(self, :id => id, :process_enter => true)
+      yield text if block_given?
+      siz.add(text)
+    }
+  end
+
+  def add_label_text(label, textid = nil)
+    btn = WX::StaticText.new(self, :label => label)
+    return with_text(btn, textid)
+  end
+
+  def add_button_text(label, buttonid = nil, textid = nil)
+    btn = WX::Button.new(self, :label => label, :id => buttonid)
+    return with_text(btn, textid) {|text|
+      btn.bind(:button) {|evt| yield evt, text } if block_given?
+    }
+  end
+
   def add_checkbox(sizer,label,id = nil, value: false, &block)
     cb = WX::CheckBox.new(self,:label => label, :id => id, :value => value)
     sizer.add(cb)
