5a2fd3cd7000b598da221ef4066da6dff7bd289f
Hans Mackowiak <hanmac@gmx.de>
2015-07-07 16:40:13 +0200

    expose selection methods from TextEntry

--- ext/wxTextEntry.hpp
+++ ext/wxTextEntry.hpp
@@ -16,4 +16,17 @@ void Init_WXTextEntry(VALUE rb_mWX);
 
 #include <wx/textentry.h>
 
+
+namespace RubyWX {
+namespace TextEntry {
+
+DLL_LOCAL VALUE _getSelection(VALUE self);
+DLL_LOCAL VALUE _setSelection(VALUE self, VALUE range);
+
+DLL_LOCAL VALUE _GetStringSelection(VALUE self);
+DLL_LOCAL VALUE _setStringSelection(VALUE self, VALUE val);
+
+}
+}
+
 #endif /* WXTEXTENTRY_HPP_ */
