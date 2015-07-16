ead2c7e5e3df05a765cc597de968a4e8e873b67d
Hans Mackowiak <hanmac@gmx.de>
2015-07-16 09:34:48 +0200

    add spec that TextAttr is dumpable and add test for switching between point and pixel size

--- spec/textattr_spec.rb
+++ spec/textattr_spec.rb
@@ -1,12 +1,25 @@
 RSpec.describe WX::TextAttr do
 
-  subject { WX::TextAttr.new(:text_color => WX::Color::BLACK) }
+  subject { WX::TextAttr.new(:text_color => WX::Color::BLACK, :font_point_size => 13) }
 
   include_examples "copyable", true
-  #include_examples "dumpable", true
+  include_examples "dumpable", true
+
+  it "switch between point and pixel size" do
+    a = WX::TextAttr.new( :font_point_size => 13 )
+    
+    expect(a.font_point_size).to eq(13)
+    expect(a.font_pixel_size).to be_nil
+
+    a.font_pixel_size = 24
+
+    expect(a.font_point_size).to be_nil
+    expect(a.font_pixel_size).to eq(24)
+
+  end
 
   it "equal hash version" do
-    should eq(:text_color => WX::Color::BLACK)
+    should eq(:text_color => WX::Color::BLACK, :font_point_size => 13)
   end
 
 end
