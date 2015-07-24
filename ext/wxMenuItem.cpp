090d17974eb3f6ac9305aa18be7262ecff6a09e9
Hans Mackowiak <hanmac@gmx.de>
2015-07-24 17:33:38 +0200

    add some fixes and make MenuItem dump and copyable

--- ext/wxMenuItem.cpp
+++ ext/wxMenuItem.cpp
@@ -25,26 +25,126 @@ namespace MenuItem {
 APP_PROTECT(wxMenuItem)
 
 macro_attr(Menu,wxMenu*)
-macro_attr(ItemLabel,wxString)
 macro_attr_with_func(Id,wrapID,unwrapID)
 macro_attr_enum(Kind,wxItemKind)
 macro_attr(SubMenu,wxMenu*)
 
 macro_attr(Help,wxString)
 
-macro_attr_bitmap(Bitmap, WRAP_BITMAP_RAISE, wxART_MENU)
+macro_attr_bitmap_func(Bitmap, WRAP_BITMAP_NULL, wxART_MENU, _self->GetKind() == wxITEM_NORMAL)
 
-/*
+singlereturn(GetItemLabel)
+
+DLL_LOCAL VALUE _setItemLabel(VALUE self,VALUE val)
+{
+	rb_check_frozen(self);
+	wxString cval(unwrap<wxString>(val));
+
+	if((!_self->IsSeparator() || !wxIsStockID(_self->GetId())) && cval.empty()){
+		rb_raise(rb_eArgError,"id '%" PRIsVALUE "' (%d) needs an text", RB_OBJ_STRING(_getId(self)), _self->GetId());
+	}
+	_self->SetItemLabel(cval);
+
+	return val;
+}
+
+/* Document-method: initialize_copy
+ * call-seq:
+ *   initialize_copy(orig)
+ *
+ * Duplicate an object
 */
 DLL_LOCAL VALUE _initialize_copy(VALUE self,VALUE other)
 {
-	_setItemLabel(self,_getItemLabel(other));
 	_setId(self,_getId(other));
-	_setKind(self,_getKind(other));
+	_setItemLabel(self,_GetItemLabel(other));
+	_self->SetKind( unwrap<wxMenuItem*>(other)->GetKind() );
 	_setSubMenu(self,_getSubMenu(other));
 	_setHelp(self,_getHelp(other));
-	_setBitmap(self,_getBitmap(other));
+	if(_self->GetKind() == wxITEM_NORMAL)
+		_setBitmap(self,_getBitmap(other));
 	return self;
+}
+
+
+/*
+ * call-seq:
+ *   hash -> Fixnum
+ *
+ * Generates a Fixnum hash value for this object.
+ *
+ *
+ */
+DLL_LOCAL VALUE _getHash(VALUE self)
+{
+	st_index_t h = rb_hash_start(0);
+
+	h = rb_hash_uint32(h, rb_str_hash(_GetItemLabel(self)));
+	h = rb_hash_uint(h, NUM2LONG(rb_hash(_getId(self))));
+	h = rb_hash_uint(h, _self->GetKind());
+	h = rb_hash_uint(h, rb_str_hash(_getHelp(self)));
+	h = rb_hash_uint(h, NUM2LONG(rb_hash(_getBitmap(self))));
+
+	h = rb_hash_end(h);
+	return LONG2FIX(h);
+}
+
+struct equal_obj {
+	wxMenuItem* self;
+	VALUE other;
+};
+
+VALUE _equal_block(equal_obj *obj)
+{
+	wxMenuItem *cother = unwrap<wxMenuItem*>(obj->other);
+
+	if(obj->self->GetId() != cother->GetId())
+		return Qfalse;
+
+	if(obj->self->GetKind() != cother->GetKind())
+		return Qfalse;
+
+	if(obj->self->GetItemLabel() != cother->GetItemLabel())
+		return Qfalse;
+
+	if(obj->self->GetHelp() != cother->GetHelp())
+		return Qfalse;
+
+	if(obj->self->IsEnabled() != cother->IsEnabled())
+		return Qfalse;
+	
+	if(obj->self->IsCheckable())
+	{
+		if(obj->self->IsChecked() != cother->IsChecked())
+			return Qfalse;
+	}
+
+	return Qtrue;
+}
+
+VALUE _equal_rescue(VALUE val)
+{
+	return Qfalse;
+}
+
+/*
+ * call-seq:
+ *   == item -> bool
+ *
+ * compares two menu items.
+ *
+ *
+ */
+DLL_LOCAL VALUE _equal(VALUE self, VALUE other)
+{
+	equal_obj obj;
+	obj.self = _self;
+	obj.other = other;
+
+	return rb_rescue(
+		RUBY_METHOD_FUNC(_equal_block),(VALUE)&obj,
+		RUBY_METHOD_FUNC(_equal_rescue),Qnil
+	);
 }
 
 /*
@@ -59,14 +159,12 @@ DLL_LOCAL VALUE _marshal_dump(VALUE self)
 {
 	VALUE result = rb_ary_new();
 
-	rb_ary_push(result,_getItemLabel(self));
 	rb_ary_push(result,_getId(self));
-	rb_ary_push(result,_getKind(self));
+	rb_ary_push(result,_GetItemLabel(self));
+	rb_ary_push(result,INT2NUM(_self->GetKind()));
 //	rb_ary_push(result,_getSubMenu(self));
 	rb_ary_push(result,_getHelp(self));
-#if wxUSE_IMAGE
 	rb_ary_push(result,_getBitmap(self));
-#endif
 	return result;
 }
 
@@ -80,14 +178,17 @@ DLL_LOCAL VALUE _marshal_dump(VALUE self)
  */
 DLL_LOCAL VALUE _marshal_load(VALUE self,VALUE data)
 {
-	_setItemLabel(self,RARRAY_AREF(data,0));
-	_setId(self,RARRAY_AREF(data,1));
+	data = rb_Array(data);
+
+	_setId(self,RARRAY_AREF(data,0));
+	_setItemLabel(self,RARRAY_AREF(data,1));
 	_setKind(self,RARRAY_AREF(data,2));
 
 	_setHelp(self,RARRAY_AREF(data,3));
-#if wxUSE_IMAGE
-	_setBitmap(self,RARRAY_AREF(data,4));
-#endif
+
+	if(_self->GetKind() == wxITEM_NORMAL)
+		_setBitmap(self,RARRAY_AREF(data,4));
+
 	return self;
 }
 
@@ -119,7 +220,7 @@ DLL_LOCAL VALUE _marshal_load(VALUE self,VALUE data)
  * returns the help string of the menu item. Type is String
  */
 /* Document-attr: bitmap
- * returns the Bitmap, currently can be copied with inititalize_copy but only stored with Marshal when wxImage is available
+ * returns the Bitmap
  */
 
 DLL_LOCAL void Init_WXMenuItem(VALUE rb_mWX)
@@ -147,10 +248,10 @@ DLL_LOCAL void Init_WXMenuItem(VALUE rb_mWX)
 
 #endif
 
-	rb_define_private_method(rb_cWXMenuItem,"initialize_copy",RUBY_METHOD_FUNC(_initialize_copy),0);
+	rb_define_private_method(rb_cWXMenuItem,"initialize_copy",RUBY_METHOD_FUNC(_initialize_copy),1);
 
 	rb_define_attr_method(rb_cWXMenuItem,"menu",_getMenu,_setMenu);
-	rb_define_attr_method(rb_cWXMenuItem,"label",_getItemLabel,_setItemLabel);
+	rb_define_attr_method(rb_cWXMenuItem,"label",_GetItemLabel,_setItemLabel);
 	rb_define_attr_method(rb_cWXMenuItem,"id",_getId,_setId);
 
 	rb_define_attr_method(rb_cWXMenuItem,"sub_menu",_getSubMenu,_setSubMenu);
@@ -160,10 +261,21 @@ DLL_LOCAL void Init_WXMenuItem(VALUE rb_mWX)
 	rb_define_attr_method(rb_cWXMenuItem,"bitmap",_getBitmap,_setBitmap);
 
 	rb_define_method(rb_cWXMenuItem,"marshal_dump",RUBY_METHOD_FUNC(_marshal_dump),0);
-	rb_define_method(rb_cWXMenuItem,"marshal_load",RUBY_METHOD_FUNC(_marshal_load),-2);
+	rb_define_method(rb_cWXMenuItem,"marshal_load",RUBY_METHOD_FUNC(_marshal_load),1);
 
+	rb_define_method(rb_cWXMenuItem,"hash",RUBY_METHOD_FUNC(_getHash),0);
+
+	rb_define_method(rb_cWXMenuItem,"==",RUBY_METHOD_FUNC(_equal),1);
+	rb_define_alias(rb_cWXMenuItem,"eql?","==");
 
 	registerInfo<wxMenuItem>(rb_cWXMenuItem);
+
+	registerEnum<wxItemKind>("wxItemKind", Qnil)
+		->add(wxITEM_SEPARATOR, "separator")
+		->add(wxITEM_NORMAL, "normal")
+		->add(wxITEM_CHECK, "check")
+		->add(wxITEM_RADIO, "radio")
+		->add(wxITEM_DROPDOWN, "dropdown");
 
 #endif
 }
