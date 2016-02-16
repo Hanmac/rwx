0c617cbcf108722c708b11e0a8f0cec60a1b0965
Hanmac <hanmac@gmx.de>
2016-02-16 08:39:10 +0100

    updated wxBitmap spec to have test cases for depth and scale

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -5,18 +5,21 @@ RSpec.describe WX::Bitmap do
   include_examples "copyable", true
   include_examples "dumpable", true
 
-  describe "with 24 bit" do
-    subject { WX::Bitmap.new(3, 4, 24) }
+  [24, 32].each do |depth|
+    describe "with #{depth} bit" do
+      subject { WX::Bitmap.new(3, 4, :depth => depth) }
 
-    include_examples "copyable", true
-    include_examples "dumpable", true
+      include_examples "copyable", true
+      include_examples "dumpable", true
+    end
+
+    [0.5, 1.0, 2.0].each do |scale|
+      describe "with #{depth} bit and #{scale} scale" do
+        subject { WX::Bitmap.new(100, 200, :depth => depth, :scale => scale) }
+
+        include_examples "copyable", true
+        include_examples "dumpable", true
+      end
+    end
   end
-
-  describe "with 32 bit" do
-    subject { WX::Bitmap.new(3, 4, 32) }
-
-    include_examples "copyable", true
-    include_examples "dumpable", true
-  end
-
 end
