32ddd79338c124e5b27f8f97e1201b33be8f652b
Hanmac <hanmac@gmx.de>
2016-02-18 10:44:28 +0100

    add spec for Bitmap with invalid size, and one to check the values (scale might not always work)

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe WX::Bitmap do
 
       include_examples "copyable", true
       include_examples "dumpable", true
+
+      it "check values" do
+        expect(subject.width).to eq(3)
+        expect(subject.height).to eq(4)
+        expect(subject.depth).to eq(depth)
+      end
     end
 
     [0.5, 1.0, 2.0].each do |scale|
@@ -19,7 +25,25 @@ RSpec.describe WX::Bitmap do
 
         include_examples "copyable", true
         include_examples "dumpable", true
+
+        it "check values" do
+          expect(subject.width).to eq(100 * scale)
+          expect(subject.height).to eq(200 * scale)
+          expect(subject.depth).to eq(depth)
+
+          if subject.scale_factor == scale
+            expect(subject.scaled_width).to eq(100)
+            expect(subject.scaled_height).to eq(200)
+          end
+        end
       end
     end
+  end
+
+  it "error on invalid" do
+    expect { WX::Bitmap.new(-10, 10) }.to raise_error(ArgumentError)
+    expect { WX::Bitmap.new(10, -10) }.to raise_error(ArgumentError)
+    expect { WX::Bitmap.new(10, 10, :scale => 0.0) }.to raise_error(ArgumentError)
+    expect { WX::Bitmap.new(10, 10, :scale => -1.0) }.to raise_error(ArgumentError)
   end
 end
