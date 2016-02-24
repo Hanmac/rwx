18cbbb2d4df44b60dac26dba29603176ae5e8301
Hanmac <hanmac@gmx.de>
2016-02-24 22:33:38 +0100

    update wxRect and wxSize spec

--- spec/rect_spec.rb
+++ spec/rect_spec.rb
@@ -1,17 +1,72 @@
+RSpec.shared_examples "shared_rect" do
 
-RSpec.describe WX::Rect do
-  subject { ::WX::Rect.new(3, 4, 30, 40) }
-  
   include_examples "copyable", true
   include_examples "dumpable", true
 
-  include_examples "struct_equal", {:x => 3, :y => 4, :width => 30, :height => 40}
-  
-  describe "with subclass" do
-    subject { class SubRect < WX::Rect; end; SubRect.new(3, 4, 30, 40) }
-    include_examples "copyable", true
-    include_examples "dumpable", true
+  include_examples "struct_equal", {:x => 5, :y => 5, :width => 10, :height => 10}
 
-    include_examples "struct_equal", {:x => 3, :y => 4, :width => 30, :height => 40}
+
+  def compare_helper(obj, data)
+    should_not eq(obj)
+    expect(obj).to_not eq(subject)
+    expect(obj.class).to eq(subject.class)
+
+    expect(obj).to eq(WX::Rect.new(*data.values))
+    expect(obj).to eq(data.values)
+    expect(obj).to eq(data)
+  end
+
+  it "check inflate" do
+    obj = subject.inflate(5)
+
+    compare_helper(obj, {:x => 0, :y => 0, :width => 20, :height => 20})
+  end
+
+  it "check deflate" do
+    obj = subject.deflate(5)
+
+    compare_helper(obj, {:x => 10, :y => 10, :width => 0, :height => 0})
+  end
+
+  it "check offset" do
+    obj = subject.offset(5, 5)
+
+    compare_helper(obj, {:x => 10, :y => 10, :width => 10, :height => 10})
+  end
+
+  it "check inflate!" do
+    obj = subject.dup
+
+    obj.inflate!(5)
+
+    compare_helper(obj, {:x => 0, :y => 0, :width => 20, :height => 20})
+  end
+
+  it "check deflate!" do
+    obj = subject.dup
+
+    obj.deflate!(5)
+    
+    compare_helper(obj, {:x => 10, :y => 10, :width => 0, :height => 0})
+  end
+
+  it "check offset!" do
+    obj = subject.dup
+
+    obj.offset!(5, 5)
+
+    compare_helper(obj, {:x => 10, :y => 10, :width => 10, :height => 10})
+  end
+end
+
+RSpec.describe WX::Rect do
+  subject { ::WX::Rect.new(5, 5, 10, 10) }
+
+  include_examples "shared_rect"
+
+  describe "with subclass" do
+    subject { class SubRect < WX::Rect; end; SubRect.new(5, 5, 10, 10) }
+
+    include_examples "shared_rect"
   end
 end
--- spec/size_spec.rb
+++ spec/size_spec.rb
@@ -1,22 +1,73 @@
+RSpec.shared_examples "shared_size" do
 
-RSpec.describe WX::Size do
-  subject { ::WX::Size.new(30,40) }
-  
   include_examples "copyable", true
   include_examples "dumpable", true
 
   include_examples "struct_equal", {:width => 30, :height => 40}
-  
-
-  describe "with subclass" do
-    subject { class SubSize < WX::Size; end; SubSize.new(30,40) }
-    include_examples "copyable", true
-    include_examples "dumpable", true
-
-    include_examples "struct_equal", {:width => 30, :height => 40}
-  end
 
   it "equal rect" do
     should eq(WX::Rect.new(1, 2, 30, 40))
   end
+
+  def compare_helper(obj, data)
+    should_not eq(obj)
+    expect(obj).to_not eq(subject)
+    expect(obj.class).to eq(subject.class)
+
+    expect(obj).to eq(WX::Size.new(*data.values))
+    expect(obj).to eq(data.values)
+    expect(obj).to eq(data)
+  end
+
+  it "scale" do
+    obj = subject.scale(2)
+
+    compare_helper(obj, {:width => 30 * 2, :height => 40 * 2})
+  end
+  
+  it "scale!" do
+    obj = subject.dup
+    obj.scale!(2)
+
+    compare_helper(obj, {:width => 30 * 2, :height => 40 * 2})
+  end
+  
+  it "inc_by" do
+    obj = subject.inc_by(2)
+
+    compare_helper(obj, {:width => 30 + 2, :height => 40 + 2})
+  end
+  
+  it "inc_by!" do
+    obj = subject.dup
+    obj.inc_by!(2)
+
+    compare_helper(obj, {:width => 30 + 2, :height => 40 + 2})
+  end
+  
+  
+  it "dec_by" do
+    obj = subject.dec_by(2)
+
+    compare_helper(obj, {:width => 30 - 2, :height => 40 - 2})
+  end
+  
+  it "dec_by!" do
+    obj = subject.dup
+    obj.dec_by!(2)
+
+    compare_helper(obj, {:width => 30 - 2, :height => 40 - 2})
+  end
+  
+end
+
+RSpec.describe WX::Size do
+  subject { ::WX::Size.new(30,40) }
+  include_examples "shared_size"
+
+  describe "with subclass" do
+    subject { class SubSize < WX::Size; end; SubSize.new(30,40) }
+    include_examples "shared_size"
+  end
+
 end
