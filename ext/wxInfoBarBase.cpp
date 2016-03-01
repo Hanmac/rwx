e7c83aaf039f093dbe91d7346d64d553e531689f
Hanmac <hanmac@gmx.de>
2016-03-01 08:56:37 +0100

    add extra documentation to WX::InfoBarBase

--- ext/wxInfoBarBase.cpp
+++ ext/wxInfoBarBase.cpp
@@ -26,22 +26,49 @@ namespace InfoBarBase {
 
 singlefunc(Dismiss)
 
+#ifdef HAVE_WXINFOBAR_GETBUTTONCOUNT
+singlereturn(GetButtonCount)
+#endif
+
+/*
+ * call-seq:
+ *   show_message(message, [icon]) -> self
+ *
+ * shows message with optional icon.
+ * ===Arguments
+ * * text String
+ * * icon Symbol
+ * ===Return value
+ * self
+*/
 DLL_LOCAL VALUE _showMessage(int argc,VALUE *argv,VALUE self)
 {
 	VALUE text,icon;
 	rb_scan_args(argc, argv, "11",&text,&icon);
-
+	rb_check_frozen(self);
 	//use argc for check if parameter is given because nil is treated different
 	_self->ShowMessage(unwrap<wxString>(text), argc == 1 ? wxICON_INFORMATION : unwrap_iconflag(icon));
 
 	return self;
 }
 
+/*
+ * call-seq:
+ *   add_button(id, [label]) {|evt| ] -> self
+ *
+ * add new button to the InfoBar
+ * ===Arguments
+ * * id Symbol
+ * * label String
+ * ===Return value
+ * self
+*/
 DLL_LOCAL VALUE _addButton(int argc,VALUE *argv,VALUE self)
 {
 	VALUE id,label;
 	rb_scan_args(argc, argv, "11",&id,&label);
 	
+	rb_check_frozen(self);
 	wxWindowID wid = unwrapID(id);
 	
 	_self->AddButton(wid,unwrap<wxString>(label));
@@ -56,8 +83,19 @@ DLL_LOCAL VALUE _addButton(int argc,VALUE *argv,VALUE self)
 	return self;
 }
 
+/*
+ * call-seq:
+ *   remove_button(id) -> self
+ *
+ * remove button from the InfoBar
+ * ===Arguments
+ * * id Symbol
+ * ===Return value
+ * self
+*/
 DLL_LOCAL VALUE _removeButton(VALUE self,VALUE id)
 {
+	rb_check_frozen(self);
 	wxWindowID wid = unwrapID(id);
 
 #ifdef HAVE_WXINFOBAR_GETBUTTONCOUNT
@@ -71,20 +109,32 @@ DLL_LOCAL VALUE _removeButton(VALUE self,VALUE id)
 
 #ifdef HAVE_WXINFOBAR_GETBUTTONCOUNT
 
+/*
+ * call-seq:
+ *   has_button?(id) -> bool
+ *
+ * returns true if InfoBar has button with the given id.
+ * ===Arguments
+ * * id Symbol
+ * ===Return value
+ * bool
+*/
 DLL_LOCAL VALUE _hasButton(VALUE self,VALUE id)
 {
 	return wrap(_self->HasButtonId(unwrapID(id)));
 }
 
-DLL_LOCAL VALUE _each_button_size(VALUE self)
-{
-	return UINT2NUM(_self->GetButtonCount());
-}
-
-
+/*
+ * call-seq:
+ *   each_button {|id|  } -> self
+ *
+ * iterate over the buttons
+ * ===Return value
+ * self
+*/
 DLL_LOCAL VALUE _each_button(VALUE self)
 {
-	RETURN_SIZED_ENUMERATOR(self,0,NULL,RUBY_METHOD_FUNC(_each_button_size));
+	RETURN_SIZED_ENUMERATOR(self,0,NULL,RUBY_METHOD_FUNC(_GetButtonCount));
 	
 	std::size_t count = _self->GetButtonCount();
 	
@@ -94,6 +144,16 @@ DLL_LOCAL VALUE _each_button(VALUE self)
 	return self;
 }
 
+/*
+ * call-seq:
+ *   get_button(idx) -> Symbol
+ *
+ * returns the id of the button at the given position.
+ * ===Arguments
+ * * idx Integer
+ * ===Return value
+ * Symbol
+*/
 DLL_LOCAL VALUE _getButton(VALUE self,VALUE idx)
 {
 	int cidx = NUM2INT(idx);
@@ -108,6 +168,16 @@ DLL_LOCAL VALUE _getButton(VALUE self,VALUE idx)
 }
 
 #endif
+
+/* Document-method: dismiss
+ * call-seq:
+ *   dismiss -> self
+ *
+ * hides the InfoBar
+ * ===Return value
+ * self
+*/
+
 DLL_LOCAL void Init_WXInfoBarBase(VALUE rb_mWX)
 {
 #if 0
@@ -129,12 +199,10 @@ DLL_LOCAL void Init_WXInfoBarBase(VALUE rb_mWX)
 	rb_define_method(rb_cWXInfoBarBase,"remove_button",RUBY_METHOD_FUNC(_removeButton),1);
 
 #ifdef HAVE_WXINFOBAR_GETBUTTONCOUNT
-
 	rb_define_method(rb_cWXInfoBarBase,"has_button?",RUBY_METHOD_FUNC(_hasButton),1);
 	rb_define_method(rb_cWXInfoBarBase,"get_button",RUBY_METHOD_FUNC(_getButton),1);
 
 	rb_define_method(rb_cWXInfoBarBase,"each_button",RUBY_METHOD_FUNC(_each_button),0);
-
 #endif
 
 	registerType<wxInfoBarBase>(rb_cWXInfoBarBase);
