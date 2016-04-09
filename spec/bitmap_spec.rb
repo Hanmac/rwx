2543fc116cb228ffd5fc67f0131ca0c2598039c4
Hanmac <hanmac@gmx.de>
2016-04-09 21:08:33 +0200

    add checks for bitmap and image

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe WX::Bitmap do
         include_examples "copyable", true
         include_examples "dumpable", true
 
-        it "check values" do
+        it ", check values" do
           expect(subject.width).to eq(100 * scale)
           expect(subject.height).to eq(200 * scale)
           expect(subject.depth).to eq(depth)
@@ -35,6 +35,38 @@ RSpec.describe WX::Bitmap do
             expect(subject.scaled_width).to eq(100)
             expect(subject.scaled_height).to eq(200)
           end
+        end
+
+        it ", scale by ratio" do
+          scaled = subject.scale(3, 2)
+
+          expect(scaled.width).to eq(3 * 100 * scale)
+          expect(scaled.height).to eq(2 * 200 * scale)
+
+          if scaled.scale_factor == scale
+            expect(scaled.scaled_width).to eq(3 * 100)
+            expect(scaled.scaled_height).to eq(2 * 200)
+          end
+
+          expect { subject.scale(3, 0) }.to raise_error(ArgumentError)
+          expect { subject.scale(0, 3) }.to raise_error(ArgumentError)
+        end
+
+        it ", scale by size" do
+          scaled = subject.scale(WX::Size.new(200, 100))
+
+          expect(scaled.width).to eq(200)
+          expect(scaled.height).to eq(100)
+          
+          expect(scaled.scale_factor).to eq(1.0)
+
+          if scaled.scale_factor == scale
+            expect(scaled.scaled_width).to eq(200)
+            expect(scaled.scaled_height).to eq(100)
+          end
+          
+          expect { subject.scale(WX::Size.new(3, -3)) }.to raise_error(ArgumentError)
+          expect { subject.scale(WX::Size.new(-3, 3)) }.to raise_error(ArgumentError)
         end
       end
     end
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,9 +1,28 @@
 RSpec.describe WX::Image do
 
-  subject { WX::Image.new(3, 4) }
+  subject { WX::Image.new(100, 200) }
 
   include_examples "copyable", true
   include_examples "dumpable", true
+
+  it "scale by ratio" do
+    [3, -3].product([2, -2]).each do |x, y|
+      scaled = subject.scale(x, y)
+
+      expect(scaled.width).to eq(x.abs * 100)
+      expect(scaled.height).to eq(y.abs * 200)
+    end
+  end
+
+  it "scale by size" do
+    scaled = subject.scale(WX::Size.new(200, 100))
+
+    expect(scaled.width).to eq(200)
+    expect(scaled.height).to eq(100)
+
+    expect { subject.scale(WX::Size.new(3, -3)) }.to raise_error(ArgumentError)
+    expect { subject.scale(WX::Size.new(-3, 3)) }.to raise_error(ArgumentError)
+  end
 
   it "error on invalid" do
     expect { WX::Image.new(-10, 10) }.to raise_error(ArgumentError)
