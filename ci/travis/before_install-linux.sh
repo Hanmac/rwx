0d8c2742e1d2e65398c0207716d8babbd791dbdc
Hanmac <hanmac@gmx.de>
2016-04-11 17:09:36 +0200

    update travis to trusty

--- ci/travis/before_install-linux.sh
+++ ci/travis/before_install-linux.sh
@@ -1,4 +1,10 @@
+codename=`lsb_release -s -c`
 sudo apt-key adv --fetch-keys http://repos.codelite.org/CodeLite.asc
+
+if [ ${codename} = 'trusty' ]; then
+sudo apt-add-repository 'deb http://repos.codelite.org/wx3.0.2/ubuntu/ trusty universe'
+else
 sudo add-apt-repository 'deb http://repos.codelite.org/wx3.0/ubuntu/ precise universe'
+fi
 sudo apt-get update
 sudo apt-get install -y libwxbase3.0-dev libwxgtk3.0-dev
