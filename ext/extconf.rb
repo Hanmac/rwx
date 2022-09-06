3d4cba5213f15208975930dc5d5f03650360a700
Mathieu Brèthes <thisisnotanemail@dontspam.com>
2022-09-06 10:34:34 +0200

    This is a commit to get this compiled with Debian Bullseye in 2022.
    Modified:
    
    - README.rdoc to add basic compilation instructions and library dependency info
    - ext/rwx.cpp to refactor some code that fails to compile due to compiler update (something about constants)
    - ext/extconf.rb with 2 changes:
      * addition of a -fpermissive flag so as to turn some other C++ errors into warnings
      * removing gcc / wx gcc similarity check because you do not actually need to use exactly the same compiler to get code working with shared libraries.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -40,14 +40,16 @@ if(wxversion = pkg_config('wx', 'version'))
 	end
 	
 	wxcc = pkg_config('wx', 'cc')
-	unless wxcc == ruby_cc
-		abort("CC compiler missmatch %s == %s" % [wxcc, ruby_cc])
-	end
+# there's no actual need to check "compiler compatibility" because Linux compilers make
+# compatible ABI calls anyway.
+#	unless wxcc == ruby_cc
+#		abort("CC compiler missmatch %s == %s" % [wxcc, ruby_cc])
+#	end
 	
 	wxcxx = pkg_config('wx', 'cxx')
-	unless wxcxx == ruby_cxx
-		abort("CXX compiler missmatch %s == %s" % [wxcxx, ruby_cxx])
-	end
+#	unless wxcxx == ruby_cxx
+#		abort("CXX compiler missmatch %s == %s" % [wxcxx, ruby_cxx])
+#	end
 	
 	#earlier versions of ruby does not have that constant
 	$CXXFLAGS = CONFIG["CXXFLAGS"] unless defined?($CXXFLAGS)
@@ -67,7 +69,7 @@ if(wxversion = pkg_config('wx', 'version'))
 		$CXXFLAGS << " " << pkg[0] if pkg && !$CXXFLAGS[pkg[0]]
 	}
 	
-	all = " -fvisibility-inlines-hidden"
+	all = " -fvisibility-inlines-hidden -fpermissive"
 	$CFLAGS << all << " -x c++ -g "
 	$CXXFLAGS << all << " -g "
 	$CPPFLAGS << all << " -g "
